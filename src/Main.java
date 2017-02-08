e967810c5a53b7713661842cf9779a70198a30d5
Andrew Harris <blakebharris@gmail.com>
2017-02-08 17:19:18 -0600

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,45 +8,24 @@ public class Main {
         Questionnaire questionnaire = new Questionnaire();
         QuestionAndAnswer qa = new QuestionAndAnswer();
         Scanner scanner = new Scanner(System.in);
-        String[] questions = {
-                "What is your favorite product?",
-                "Why is this product your favorite?",
-                "If you could change one thing, what would it be?",
-                "In what ways do you find yourself using the product?",
-                "Do you want to delete this product immediately?"
-        };
 
         try{
             Questionnaire.loadQuestionnaire();
-            int count = 1;
-            for(Questionnaire q : QuestionAndAnswer.questionAnswers) {
-                System.out.printf("Q%d: %s\n",count, q.getQuestion());
-                System.out.printf("A%d: %s\n",count, q.getAnswer());
-                System.out.println();
-                count++;
+            Questionnaire.printQuestionAndAnswers(scanner);
+
+            System.out.println("Would you like to change any answers?");
+            String userAnswer = scanner.nextLine().toLowerCase();
+            if(userAnswer.equals("y")) {
+                boolean isTrue = true;
+                while(isTrue) {
+                    isTrue = Questionnaire.updateAnswers(scanner);
+                }
             }
         }catch(Exception e){
             System.out.println("I see you have not done this survey before.");
+            questionnaire.answerSurveyQuestions(scanner);
+
         }
-
-        System.out.println("Would you like to change any answers?");
-
-        String userAnswer = scanner.nextLine().toLowerCase();
-        if(userAnswer.equals("y")) {
-            boolean isTrue = true;
-            while(isTrue) {
-                isTrue = Questionnaire.updateAnswers(scanner);
-            }
-        }else{
-            for (String q : questions) {
-
-                System.out.println(q);
-                String answer = scanner.nextLine();
-                Questionnaire qA = new Questionnaire(q, answer);
-                questionnaire.storeQuestionAndAnswer(qA);
-            }
-        }
-
         System.out.println("Would you like to save your answers? [y/n] ");
         String a = scanner.nextLine();
         if(a.equals("y")) {
--- src/Questionnaire.java
+++ src/Questionnaire.java
@@ -11,9 +11,7 @@ public class Questionnaire {
     String answer;
 
 
-    public Questionnaire(){
-
-    }
+    public Questionnaire(){}
 
     public Questionnaire(String question, String answer) {
         this.question = question;
@@ -86,5 +84,32 @@ public class Questionnaire {
             isTrue = false;
         }
         return isTrue;
+    }
+
+    public static void printQuestionAndAnswers(Scanner scanner) {
+        int count = 1;
+        for(Questionnaire q : QuestionAndAnswer.questionAnswers) {
+            System.out.printf("Q%d: %s\n",count, q.getQuestion());
+            System.out.printf("A%d: %s\n",count, q.getAnswer());
+            System.out.println();
+            count++;
+        }
+    }
+
+    public void answerSurveyQuestions(Scanner scanner) {
+        String[] questions = {
+                "What is your favorite product?",
+                "Why is this product your favorite?",
+                "If you could change one thing, what would it be?",
+                "In what ways do you find yourself using the product?",
+                "Do you want to delete this product immediately?"
+        };
+        for (String q : questions) {
+
+            System.out.println(q);
+            String answer = scanner.nextLine();
+            Questionnaire qA = new Questionnaire(q, answer);
+            storeQuestionAndAnswer(qA);
+        }
     }
 }
